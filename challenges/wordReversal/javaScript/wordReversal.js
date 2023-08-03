31912775e79b5a6e193f41527d3d6e2ff535d48b
Kyle Parkin <13ktparkin@gmail.com>
2023-08-03 15:31:00 +0100

    worked out details of problem

--- challenges/wordReversal/javaScript/wordReversal.js
+++ challenges/wordReversal/javaScript/wordReversal.js
@@ -1,4 +1,16 @@
+/*
 
+Understanding the problem:
+take in a string of of a sentence
+output the string as a sentence but with each word spelled backwards 
+
+Plan:
+initiate an array
+turn the string into an array seperated by each word
+loop through the array taking each index and looping through it backwords pushing it into the array.
+turn the new array into a string and return it.
+
+*/
 
 
 function reverseWords(sentence){
